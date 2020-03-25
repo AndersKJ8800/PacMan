0e617758612dff4e2410f95b92d370511ba6f205
AndersKJ8800 <58813668+AndersKJ8800@users.noreply.github.com>
2020-03-25 14:57:20 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
+let resX = 224;
+let resY = 288;
 
+function setup() {
+  createScaleCanvas();
+  frameRate(60);
+}
+
+function windowResized() { createScaleCanvas(); }
+
+function draw() {
+  scale(scaling);
+  background(0);
+}
