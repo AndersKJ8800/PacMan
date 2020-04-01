d8c45add6afde1b45056974e8b0b003f3961b1ee
AndersKJ8800 <AndersKJ8800@gmail.com>
2020-04-01 02:09:17 +0200

    asdf

--- preload.js
+++ preload.js
@@ -1,77 +1,76 @@
-let texture;
 let sprite;
 let img;
 
 function preload() {
 
   img = {
-    defines: loadImage('resources/assets/map_defines.png'),
-    background: loadImage('resources/assets/background.png')
+    defines: loadImage('resources/images/map_defines.png'),
+    background: loadImage('resources/images/background.png')
   }
 
   sprite = {
 
     pac_man: [
-      loadImage('resources/assets/placeholder.png'),
-      loadImage('resources/assets/placeholder.png'),
-      loadImage('resources/assets/placeholder.png'),
-      loadImage('resources/assets/placeholder.png'),
-      loadImage('resources/assets/placeholder.png'),
-      loadImage('resources/assets/placeholder.png'),
-      loadImage('resources/assets/placeholder.png'),
-      loadImage('resources/assets/placeholder.png'),
-      loadImage('resources/assets/placeholder.png')
+      loadImage('resources/images/placeholder.png'),
+      loadImage('resources/images/placeholder.png'),
+      loadImage('resources/images/placeholder.png'),
+      loadImage('resources/images/placeholder.png'),
+      loadImage('resources/images/placeholder.png'),
+      loadImage('resources/images/placeholder.png'),
+      loadImage('resources/images/placeholder.png'),
+      loadImage('resources/images/placeholder.png'),
+      loadImage('resources/images/placeholder.png')
     ],
 
     big_pac_man: [
-      loadImage('resources/assets/placeholder.png'),
-      loadImage('resources/assets/placeholder.png'),
-      loadImage('resources/assets/placeholder.png')
+      loadImage('resources/images/placeholder.png'),
+      loadImage('resources/images/placeholder.png'),
+      loadImage('resources/images/placeholder.png')
     ],
 
     fruit: [
-      loadImage('resources/assets/cherry.png'),
-      loadImage('resources/assets/strawberry.png'),
-      loadImage('resources/assets/orange.png'),
-      loadImage('resources/assets/apple.png'),
-      loadImage('resources/assets/melon.png'),
-      loadImage('resources/assets/galaxian.png'),
-      loadImage('resources/assets/bell.png'),
-      loadImage('resources/assets/key.png')
+      loadImage('resources/images/cherry.png'),
+      loadImage('resources/images/strawberry.png'),
+      loadImage('resources/images/orange.png'),
+      loadImage('resources/images/apple.png'),
+      loadImage('resources/images/melon.png'),
+      loadImage('resources/images/galaxian.png'),
+      loadImage('resources/images/bell.png'),
+      loadImage('resources/images/key.png')
     ],
 
-    dot: loadImage('resources/assets/dot.png'),
-    power_pellet: loadImage('resources/assets/power_pellet.png'),
-    gate: loadImage('resources/assets/gate.png'),
+    dot: loadImage('resources/images/dot.png'),
+    power_pellet: loadImage('resources/images/power_pellet.png'),
+    gate: loadImage('resources/images/gate.png'),
 
     ghost_body: [
-      loadImage('resources/assets/ghost_body_0.png'),
-      loadImage('resources/assets/ghost_body_1.png')
+      loadImage('resources/images/ghost_body_0.png'),
+      loadImage('resources/images/ghost_body_1.png')
     ],
 
     ghost_eyes: [
-      loadImage('resources/assets/ghost_eyes_left.png'),
-      loadImage('resources/assets/ghost_eyes_up.png'),
-      loadImage('resources/assets/ghost_eyes_right.png'),
-      loadImage('resources/assets/ghost_eyes_down.png')
+      loadImage('resources/images/ghost_eyes_left.png'),
+      loadImage('resources/images/ghost_eyes_up.png'),
+      loadImage('resources/images/ghost_eyes_right.png'),
+      loadImage('resources/images/ghost_eyes_down.png')
     ],
 
     ghost_score: [
-      loadImage('resources/assets/score_200.png'),
-      loadImage('resources/assets/score_400.png'),
-      loadImage('resources/assets/score_800.png'),
-      loadImage('resources/assets/score_1600.png')
+      loadImage('resources/images/score_200.png'),
+      loadImage('resources/images/score_400.png'),
+      loadImage('resources/images/score_800.png'),
+      loadImage('resources/images/score_1600.png')
     ],
 
     fruit_score: [
-      loadImage('resources/assets/score_100.png'),
-      loadImage('resources/assets/score_300.png'),
-      loadImage('resources/assets/score_500.png'),
-      loadImage('resources/assets/score_700.png'),
-      loadImage('resources/assets/score_1000.png'),
-      loadImage('resources/assets/score_2000.png'),
-      loadImage('resources/assets/score_3000.png'),
-      loadImage('resources/assets/score_5000.png')
+      loadImage('resources/images/score_100.png'),
+      loadImage('resources/images/score_300.png'),
+      loadImage('resources/images/score_500.png'),
+      loadImage('resources/images/score_700.png'),
+      loadImage('resources/images/score_1000.png'),
+      loadImage('resources/images/score_2000.png'),
+      loadImage('resources/images/score_3000.png'),
+      loadImage('resources/images/score_5000.png')
     ]
 
   }
--- scripts/scenes/game.js
+++ scripts/scenes/game.js
@@ -1,17 +1,19 @@
 function game() {
 
+  //tegn bagrunds labyrint
   image(img.background, 0, 24);
-
+  //farv den blå
   blendMode(DARKEST);
   fill(0,0,255);
   noStroke();
   rect(0, 24, resX, 31 * 8);
   blendMode(BLEND);
 
-
+  //opdatèr alle dots
   for (let i = 0; i < squares.length; i++) {
-    squares[i].changeType();
-    squares[i].display();
+    if (squares[i].type === "dot" || "power pellet") {
+      squares[i].display();
+    }
   }
 
 
